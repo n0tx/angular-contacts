48419ee48ecaf331fc18f5e3daed1b7c141b7589
n0tx <rcandra91@msn.com>
2025-06-10 18:31:28 +0700

    switch material theme pada angular.json menambahkan mat-toolbar pada app komponen menambahkan mat-icon pada app komponen menambahkan css pada mat-toolbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
     selector: "app-root",
-    imports: [RouterOutlet, MatSlideToggleModule],
+    imports: [
+      RouterOutlet, 
+      MatSlideToggleModule, 
+      MatToolbarModule,
+      MatIconModule
+    ],
     templateUrl: "./app.html",
     styleUrl: "./app.css"
 })
 export class App {
-    protected title = "angular-contacts";
+    protected title = "My Contacts";
 }
