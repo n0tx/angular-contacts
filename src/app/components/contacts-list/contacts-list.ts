c0c398409845d2dd49087a7da2f674302feacff4
n0tx <rcandra91@msn.com>
2025-06-10 19:52:21 +0700

    membuat interface contact membuat data signal harcoded pada komponen contacts-list.ts menampilkan data signal ke komponen contacts-list.html dengan for loop menggunakan mat-list material pada komponen contacs-list

--- src/app/components/contacts-list/contacts-list.ts
+++ src/app/components/contacts-list/contacts-list.ts
@@ -1,11 +1,27 @@
 import { Component, signal } from "@angular/core";
+import { Contact } from "../../model/contact";
+import { MatListModule } from "@angular/material/list";
 
 @Component({
     selector: "app-contacts-list",
-    imports: [],
+    imports: [MatListModule],
     templateUrl: "./contacts-list.html",
     styleUrl: "./contacts-list.css"
 })
 export class ContactsList {
     contactsListMessage = signal("contacts-list works!");
+    contacts = signal<Array<Contact>>([
+        {
+            id: "1",
+            name: "Riki",
+            email: "riki@mail.com",
+            phone: "1111111"
+        },
+        {
+            id: "2",
+            name: "Candra",
+            email: "candra@mail.com",
+            phone: "2222222"
+        }
+    ]);
 }
